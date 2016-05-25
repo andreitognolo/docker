0b32ca40f1b585d8423e1e917c0aef99babb456b
Andrei Tognolo <andreitognolo@gmail.com>
2016-05-25 18:27:33 -0300

    Update push_last_image.sh

--- push_last_image.sh
+++ push_last_image.sh
@@ -1,7 +1,6 @@
 set -ex
 
 sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker build .
 IMAGE_ID=$(docker ps -l 2> /dev/null|grep -v CONTAINER|grep -v NAMES|awk '{print $2;}')
 echo Pushing container IMAGE_ID
 docker tag $IMAGE_ID $DOCKER_USER/$IMAGE_ID
