d8e85ec063ea7cf4e2643ea4891752552b92aa37
Andrei Tognolo <andreitognolo@gmail.com>
2016-05-25 18:52:12 -0300

    Update push_last_image.sh

--- push_last_image.sh
+++ push_last_image.sh
@@ -3,5 +3,5 @@ set -ex
 sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 IMAGE_ID=$(docker ps -l 2> /dev/null|grep -v CONTAINER|grep -v NAMES|awk '{print $2;}')
 echo Pushing container $IMAGE_ID
-docker tag -f $IMAGE_ID:$SNAP_PIPELINE_COUNTER $DOCKER_USER/$IMAGE_ID
-docker push $DOCKER_USER/$IMAGE_ID:$SNAP_PIPELINE_COUNTER
+docker tag -f $IMAGE_ID $DOCKER_USER/$IMAGE_ID
+docker push $DOCKER_USER/$IMAGE_ID
