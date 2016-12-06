d0a4445ce44d70d7481a9aa3331c4317eb1f981b
Kyle Olivo <kyle@kyleolivo.com>
2016-12-06 14:07:52 -0800

    modify script

--- multigit.sh
+++ multigit.sh
@@ -1,3 +1,7 @@
-openssl rand -base64 6 > README.md
-git add README.md
-git commit -m "some update"
+#!/usr/bin/env bash
+for i in {1..500}
+do
+    openssl rand -base64 6 > README.md
+    git add README.md
+    git commit -m "some update"
+done
